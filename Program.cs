3978a382edb6ca89c8f42c883481662178ac7104
Your Name <you@example.com>
2022-02-05 21:42:55 -0300

    Commit inicial do projeto

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace nuspecgen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = args[0];
+            if (a == "1")
+            {
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+}
